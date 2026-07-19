1eb3a6165f0dee9086f9e60bb1abc1b98411f980
techansolver@gmial.com <techansolver@gmail.com>
2026-07-19 21:17:50 +0530

    Added auth file

---
+++ scripts/authorize.js
@@ -0,0 +1,105 @@
+/* 
+    (needs patch) 
+    IMPLEMENTATION OF AUTHENTICATION ROUTE AFTER REDIRECT FROM GITHUB.
+*/
+const localAuth = {
+  /**
+   * Initialize
+   */
+  init() {
+    this.KEY = 'leethub_token';
+    this.ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+    this.AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize';
+    this.CLIENT_ID = 'Enter your client id from github';
+    this.CLIENT_SECRET = 'Enter your client secret from github';
+    this.REDIRECT_URL = 'https://github.com/'; // for example, https://github.com
+    this.SCOPES = ['repo'];
+  },
+
+  /**
+   * Parses Access Code
+   *
+   * @param url The url containing the access code.
+   */
+  parseAccessCode(url) {
+    if (url.match(/\?error=(.+)/)) {
+      BrowserUtil.instance.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var tab = tabs[0];
+        BrowserUtil.instance.tabs.remove(tab.id, function () {});
+      });
+    } else {
+      this.requestToken(url.match(/\?code=([\w\/\-]+)/)[1]);
+    }
+  },
+
+  /**
+   * Request Token
+   *
+   * @param code The access code returned by provider.
+   */
+  requestToken(code) {
+    const that = this;
+    const data = new FormData();
+    data.append('client_id', this.CLIENT_ID);
+    data.append('client_secret', this.CLIENT_SECRET);
+    data.append('code', code);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          that.finish(xhr.responseText.match(/access_token=([^&]*)/)[1]);
+        } else {
+          BrowserUtil.instance.runtime.sendMessage({
+            closeWebPage: true,
+            isSuccess: false,
+          });
+        }
+      }
+    });
+    xhr.open('POST', this.ACCESS_TOKEN_URL, true);
+    xhr.send(data);
+  },
+
+  /**
+   * Finish
+   *
+   * @param token The OAuth2 token given to the application from the provider.
+   */
+  finish(token) {
+    /* Get username */
+    // To validate user, load user object from GitHub.
+    const AUTHENTICATION_URL = 'https://api.github.com/user';
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const username = JSON.parse(xhr.responseText).login;
+          BrowserUtil.instance.runtime.sendMessage({
+            closeWebPage: true,
+            isSuccess: true,
+            token,
+            username,
+            KEY: this.KEY,
+          });
+        }
+      }
+    });
+    xhr.open('GET', AUTHENTICATION_URL, true);
+    xhr.setRequestHeader('Authorization', `token ${token}`);
+    xhr.send();
+  },
+};
+
+localAuth.init(); // load params.
+const link = window.location.href;
+
+/* Check for open pipe */
+if (window.location.host === 'github.com') {
+  BrowserUtil.instance.storage.local.get('pipe_leethub', data => {
+    if (data && data.pipe_leethub) {
+      localAuth.parseAccessCode(link);
+    }
+  });
+}
---
+++ scripts/oauth2.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+const oAuth2 = {
+  /**
+   * Initialize
+   */
+  init() {
+    this.KEY = 'leethub_token';
+    this.ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+    this.AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize';
+    this.CLIENT_ID = 'Enter your client id from github';
+    this.CLIENT_SECRET = 'Enter your client secret from github';
+    this.REDIRECT_URL = 'https://github.com/'; // for example, https://github.com
+    this.SCOPES = ['repo'];
+  },
+
+  /**
+   * Begin
+   */
+  begin() {
+    this.init(); // secure token params.
+
+    let url = `${this.AUTHORIZATION_URL}?client_id=${this.CLIENT_ID}&redirect_uri${this.REDIRECT_URL}&scope=`;
+
+    for (let i = 0; i < this.SCOPES.length; i += 1) {
+      url += this.SCOPES[i];
+    }
+
+    BrowserUtil.instance.storage.local.set({ pipe_leethub: true }, () => {
+      // opening pipe temporarily, redirects to github
+      browser.tabs.create({ url, active: true }, function () {});
+    });
+  },
+};
